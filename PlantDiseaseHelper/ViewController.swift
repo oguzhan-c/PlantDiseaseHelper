1e84b1c8c8cf5c00f20158611ccf30792608da01
Oğuzhan Can <oguzhan.can.iletisim@gmail.com>
2023-02-27 13:22:15 +0300

    Initial Commit

---
+++ PlantDiseaseHelper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlantDiseaseHelper
+//
+//  Created by Oğuzhan Can on 27.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
